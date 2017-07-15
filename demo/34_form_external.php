f3ffdf668915d41fb95ec71a307a30b36f9f9b10
judeharding <jharding@genesisgroupusa.com>
2017-07-15 17:28:28 -0400

    finished sec 6

--- demo/34_form_external.php
+++ demo/34_form_external.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-    <form action="form_process.php" method="post">
+    <form action="34_form_process.php" method="post">
 
         <input type="text" name="username" placeholder="Enter Username">
         <input type="password"  name="password" placeholder="Enter Password"><br>
--- demo/_practical-app/6.php
+++ demo/_practical-app/6.php
@@ -2,28 +2,38 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
-
-		<aside class="col-xs-4">
-		
-		<?php Navigation();?>
-			
-		</aside><!--SIDEBAR-->
-
+<section class="content">
+<aside class="col-xs-4">
+	<?php Navigation();?>
+</aside><!--SIDEBAR-->
 
 <article class="main-content col-xs-8">
- 
 
-	<?php  
+<?php
+	if(isset($_POST['submit'])) {
+	    $username = $_POST['username'];
+		echo $username . ",   " . "YES it works";
+	}
 
-/*  Step1: Make a form that submits one value to POST super global
-
-
- */
-
-	
 ?>
 
+	<form action="6.php" method="post">
+		<input type="text" name="username"><br>
+		<input type="submit" name="submit">
+	</form>
+
+	<?php
+	echo "HI";
+	/*  Step1: Make a form that submits one value to POST super global*/
+
+	?>
 
 </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php" ?>
+
+<html>
+    <form action="6.php" method="post">
+        <input type="text" name="username"><br>
+        <input type="submit" name="submit">
+    </form>
+</html>
