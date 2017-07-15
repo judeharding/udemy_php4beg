582f7c159b6ba1b316b0c48d932a43e1c3914ca4
judeharding <jharding@genesisgroupusa.com>
2017-07-15 17:11:32 -0400

    working on 31-37

--- demo/34_form_process.php
+++ demo/34_form_process.php
@@ -1,46 +1,25 @@
-<?php 
+<?php
 
-if(isset($_POST['submit'])) {
-    
-$name = array("Edwin", "Student", "Peter", "Samid", "Mohad", "Maria", "Jane", "tom");
-    
-    $minimun = 5;
-    $maximun = 10;
+    if(isset($_POST['submit'])) {
+        $name = array("Edwin", "Student", "Peter", "Samid", "Mohad", "Maria", "Jane", "tom");
+        $minimun = 5;
+        $maximun = 10;
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-    
-  if(strlen($username) < $minimun ) {
-  
-      echo "Username has to be longer than five";
-  
-  }  
-    
-    if(strlen($username) > $maximun  ) {
-  
-      echo "Username cannot be longer than 10 ";
-  
-  }  
-    
-    if(!in_array($username,$name)) {
-    
-        echo " Sorry you are not allowed";
-    
-    } else {
-    
-    echo "Welcome";
+        if(strlen($username) < $minimun ) {
+          echo "Username has to be longer than five";
+        }
+
+        if(strlen($username) > $maximun  ) {
+            echo "Username cannot be longer than 10 ";
+        }
+
+        if(!in_array($username,$name)) {
+            echo " Sorry you are not allowed";
+        } else {
+            echo "Welcome";
+        }
     }
-    
-    
-    
-    
-    
-//echo "Hello" . $username;   
-//echo "Your Password is " . $password;
-
-
-}
-
-
 
 ?>
