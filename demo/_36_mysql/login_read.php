050848d4aab5d2abef62b5f9118fcab2f32ad559
judeharding <jharding@genesisgroupusa.com>
2017-07-17 16:37:02 -0400

    up to 45

--- demo/_36_mysql/login_read.php
+++ demo/_36_mysql/login_read.php
@@ -4,13 +4,15 @@
 <?php include "includes/header.php" ?>
 
 <div class="container">
-    
+
     <div class="col-sm-6">
 
     <pre>
   <?php readRows(); ?>
      </pre>
     </div>
+
+
 
 
 <?php include "includes/footer.php"?>
--- demo/_36_mysql/login_update.php
+++ demo/_36_mysql/login_update.php
@@ -6,36 +6,25 @@
 <?php include "includes/header.php" ?>
 
 <div class="container">
-
     <div class="col-sm-6">
         <h1 class="text-center">Update</h1>
-     <form action="login_update.php" method="post">
+        <form action="login_update.php" method="post">
             <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" name="username" class="form-control">
+                <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
             </div>
 
-             <div class="form-group">
+            <div class="form-group">
                 <label for="password">Password</label>
-            <input type="password" name="password" class="form-control">
+                <input type="password" name="password" class="form-control">
             </div>
 
-             <div class="form-group">
-
-             <select name="id" id="">
-                <?php
-                showAllData();
-                  ?>
-             </select>
-
-             </div>
-
+            <div class="form-group">
+                <select name="id" id="">
+                <?php showAllData(); ?>
+                </select>
+            </div>
             <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
-
         </form>
-
-
     </div>
-
-
 <?php include "includes/footer.php"?>
