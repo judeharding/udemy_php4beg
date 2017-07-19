9a150326d57eb79dea5c282c3332fca58d738591
judeharding <jharding@genesisgroupusa.com>
2017-07-19 15:27:53 -0400

    found the error

--- demo/_36_mysql/login_update.php
+++ demo/_36_mysql/login_update.php
@@ -28,7 +28,7 @@
 <body>
     <div class="container">
         <div class="col-sm-6">
-            <form class="" action="login_create.php" method="post">
+            <form class="" action="login_update.php" method="post">
                 <div class="form-group">
                     <label for="username">Username</label>
                     <input type="text" name="username" class="form-control">
