1cfd1558800793d247b00595ff39433e622021e6
judeharding <jharding@genesisgroupusa.com>
2017-07-19 12:48:12 -0400

    connecting to db

--- demo/_36_mysql/login_update.php
+++ demo/_36_mysql/login_update.php
@@ -1,15 +1,6 @@
-<?php
-include "db.php";
+<?php include "db.php";?>
+<?php include "functions.php";?>
 
-            // inserting into database
-            $query = "SELECT * FROM users";
-            // establishing the connection for the data entered
-            $result = mysqli_query($connection, $query);
-            // testing to see if the result is actually generated
-            if (!$result) {
-                die('QUERY FAILED' . mysqli_error);
-            }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,12 +27,7 @@ include "db.php";
                 </div>
                 <div class="form-group">
                     <select class="" name="id" id="">
-                        <?php
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                $id = $row['id'];
-                                echo "<option value='$id'>$id</option>";
-                            }
-                        ?>
+                        <?php showAllData(); ?>
                         <option value=""></option>
                     </select>
                 </div>
