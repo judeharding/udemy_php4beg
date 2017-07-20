ab060bc6b72802f879a3a4ac6c1d9e4a37c70442
judeharding <jharding@genesisgroupusa.com>
2017-07-20 16:29:48 -0400

    starting ch 9

--- demo/57_get.php
+++ demo/57_get.php
@@ -1,11 +1,6 @@
-<?php 
-
- print_r($_GET);
-
-
+<?php
+    print_r($_GET);
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,21 +8,15 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+
 <body>
 
-<?php 
-
+<?php
 $id = 10;
 $button = "CLICK HERE NOW";
-    
-    
 ?>
 
-
 <a href="get.php?id=<?php echo $id;?>"><?php echo $button;?></a>
-
-
-
 
 </body>
 </html>
--- demo/_practical-app/8.php
+++ demo/_practical-app/8.php
@@ -1,37 +1,40 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+<aside class="col-xs-4">
+	<?php Navigation();?>
+</aside><!--SIDEBAR-->
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+<article class="main-content col-xs-8">
 
-
-		
-	<article class="main-content col-xs-8">
-	
-	
-	<?php  
-
+<?php
 	/*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
+	Step 2 - Use crypt() function to encrypt it
+	Step 3 - Assign the crypt result to a variable
+	Step 4 - echo the variable
 	*/
-	
-	?>
 
+	// WITH SALT & HASH
+	// $somVar = "sometextasvalue";
+	//
+	// $hashFormat = "$2y$10$";
+	// $salt = "iusesomecrazystrings22";
+	// $hashF_and_salt = $hashFormat . $salt;
+	// $cryptVar = crypt($somVar,$hashF_and_salt);
+	//
+	// echo $cryptVar;
+	//
+	// OR
+	// ONLY salt
+	$somVar = "sometextasvalue";
+	$salt = "iusesomecrazystrings22";
+	$cryptVar = crypt($somVar,$salt);
 
+	echo $cryptVar;
 
-
+?>
 
 </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php" ?>
