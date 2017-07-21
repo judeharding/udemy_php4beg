c4ae4cb764995581bb1f7c9aa361e40fb098b906
judeharding <jharding@genesisgroupusa.com>
2017-07-21 18:10:37 -0400

    cleaned up code

--- demo/_practical-app/10.php
+++ demo/_practical-app/10.php
@@ -9,7 +9,6 @@
 <article class="main-content col-xs-8">
 
 <?php
-
 /*  Step 1: Use the Make a class called Dog
 Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
 Step 4: Make a method named ShowAll that echos all the properties
@@ -25,7 +24,7 @@ Step 6: Call the method ShowAll
 		var $furColor = "white";
 		//
 		function ShowAll(){
-			echo $this->eyeColor;
+			echo $this->eyeColor . "<br>";
 			echo $this->furColor;
 		}
 	}
@@ -33,15 +32,11 @@ $pitbull = new Dog();
 
 echo $pitbull->ShowAll();
 
-
-//
-//
 // if(class_exists("Dog")) {
 // 	echo "YEayyyyyyyyy Nice";
 // } else {
 // 	echo "no";
 // }
-
 ?>
 </article><!--MAIN CONTENT-->
 
